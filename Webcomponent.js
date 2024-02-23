d8e3ba4eae9f814cf340ca0fc00f410b5bea4b58
datobser <159797954+datobser@users.noreply.github.com>
2024-02-23 13:29:23 +0100

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -16,7 +16,9 @@
                 /* Center the polaroid */
                 padding: 15px;
                 background-color: white;
+                border: black;
                 box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+                
             }
         </style>
         </head>
