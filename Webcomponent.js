4d7aaf1df961aab0741d4e11a06f05d88217124f
datobser <159797954+datobser@users.noreply.github.com>
2024-02-23 12:05:59 +0100

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -10,6 +10,8 @@
                  #polaroid{
                      width: 100%;
                      height: 100%;
+                     background-color: #007bff;
+                     color: white;
                      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
                  }
                  
