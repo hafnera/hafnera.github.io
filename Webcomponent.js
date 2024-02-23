f39330e4c41a053d24d0c087e27d55859e006aab
datobser <159797954+datobser@users.noreply.github.com>
2024-02-23 10:52:04 +0100

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -12,7 +12,7 @@
                      border:1px solid #d3d3d3;
                      width: 100%;
                      height: 100%;
-                     shadow: rgb(128,128,128);
+                     shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
                  }
                  
              </style>
