9686282cc58ae0abdd31447168a3707e29080930
datobser <159797954+datobser@users.noreply.github.com>
2024-02-23 16:25:58 +0100

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -1,4 +1,8 @@
-<!DOCTYPE html>
+(function () {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML =
+       `
+       <!DOCTYPE html>
 <html>
 
 <head>
@@ -37,3 +41,33 @@
 </body>
 
 </html>
+       `;
+    
+
+    class PerformanceHelp extends HTMLElement {
+        constructor() {
+            super();
+            this.init();
+        }
+
+        init() {
+
+            let shadowRoot = this.attachShadow({ mode: "open" });
+            shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this.addEventListener("click", event => {
+                var event = new Event("onClick");
+                this.fireChanged();
+                this.dispatchEvent(event);
+            });
+
+        }
+
+        fireChanged() {
+            console.log("OnClick Triggered");
+
+        }
+
+    }
+
+    customElements.define('custom-button', PerformanceHelp);
+})();
