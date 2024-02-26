14ea191ed7c0f1b1733523db2e03afd5ac37022a
datobser <159797954+datobser@users.noreply.github.com>
2024-02-26 14:24:33 +0100

    Update styling.js

--- styling.js
+++ styling.js
@@ -3,13 +3,7 @@
     template.innerHTML = `
         <head>
             <style>
-                #form {
-                    border: 3px solid green;
-                    background: rgba(152,165,204,0.5);
-                    -webkit-backdrop-filter: blur(10px);
-                    backdrop-filter: blur(10px);
-                    border: 1px solid rgba(152,165,204,0.25);
-                }
+                
             </style>
         </head>
         <body>
@@ -34,32 +28,24 @@
     class StylingPanel extends HTMLElement {
         constructor() {
             super();
-            this._shadowRoot = this.attachShadow({mode: "open"});
-            this._shadowRoot.appendChild(template.content.cloneNode(true));
-            this._shadowRoot.getElementById("form").addEventListener("input", this._updateProperties.bind(this));
+            this.init();
         }
 
-        _updateProperties() {
-            const shadowValue = this._shadowRoot.getElementById("shadow_input").checked;
-            const sizeValue = this._shadowRoot.getElementById("size_input").value;
-            this.dispatchEvent(new CustomEvent("propertiesChanged", {
-                detail: {
-                    properties: {
-                        shadow: shadowValue,
-                        size: sizeValue
-                    }
-                }
-            }));
+        _init() {
+         let shadowRoot = this.attachShadow({ mode: "open" });
+            shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this.addEventListener("click", event => {
+                var event = new Event("onClick");
+                this.fireChanged();
+                this.dispatchEvent(event);
+            });
+
         }
 
-        set shadow(value) {
-            this._shadowRoot.getElementById("shadow_input").checked = value;
-        }
-
-        set size(value) {
-            this._shadowRoot.getElementById("size_input").value = value;
-        }
-    }
-
+         fireChanged() {
+                console.log("OnClick Triggered");
+    
+            }
+        
     customElements.define("custom-button-styling", StylingPanel);
 })();
