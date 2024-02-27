f297c5d082a08bd6301a6a0f6fe7986afcd1ba03
datobser <159797954+datobser@users.noreply.github.com>
2024-02-27 13:07:49 +0100

    Update mouse.js

--- mouse.js
+++ mouse.js
@@ -1,53 +1,65 @@
 (function () {
-    const template = document.createElement('template')
-    template.innerHTML = `
-      <style>
-        /* CSS-Stile für das Widget */
-        #root {
-          width: 200px;
-          height: 100px;
-          background-color: #f0f0f0;
-          border: 1px solid #ccc;
-          padding: 10px;
-          box-sizing: border-box;
-        }
-      </style>
-      <div id="root">
-        <!-- Inhalt des Widgets -->
-        Hover over me!
-      </div>
-      `
+  const template = document.createElement('template')
+  template.innerHTML = `
+    <style>
+      /* CSS styles for the widget */
+      #root {
+        width: 200px;
+        height: 100px;
+        background-color: #f0f0f0;
+        border: 1px solid #ccc;
+        padding: 10px;
+        box-sizing: border-box;
+        cursor: pointer; /* Add pointer cursor to indicate clickability */
+      }
+    </style>
+    <div id="root">
+      <!-- Widget content -->
+      Click me!
+    </div>
+  `
 
-    class CustomWidget extends HTMLElement {
-        constructor() {
-            super()
+  class CustomWidget extends HTMLElement {
+    constructor () {
+      super()
 
-            // Schatten-DOM initialisieren und Template hinzufügen
-            this._shadowRoot = this.attachShadow({ mode: 'open' })
-            this._shadowRoot.appendChild(template.content.cloneNode(true))
+      // Initialize shadow DOM and add template
+      this._shadowRoot = this.attachShadow({ mode: 'open' })
+      this._shadowRoot.appendChild(template.content.cloneNode(true))
 
-            // Event-Listener für onMouseOver und onMouseOut hinzufügen
-            const rootElement = this._shadowRoot.getElementById('root')
-            rootElement.addEventListener('mouseover', this._handleMouseOver.bind(this))
-            rootElement.addEventListener('mouseout', this._handleMouseOut.bind(this))
-        }
-
-        // Event-Handler für onMouseOver
-        _handleMouseOver(event) {
-            // Aktionen ausführen, wenn Maus über das Widget bewegt wird
-            console.log('Mouse over the widget!')
-            // Beispiel: Stiländerung bei Mouseover
-            event.target.style.backgroundColor = '#ccc'
-        }
-
-        // Event-Handler für onMouseOut
-        _handleMouseOut(event) {
-            // Aktionen ausführen, wenn Maus das Widget verlässt
-            console.log('Mouse out of the widget!')
-            // Beispiel: Zurücksetzen der Stiländerung bei Mouseout
-            event.target.style.backgroundColor = '#f0f0f0'
-        }
+      // Add event listeners for onClick, onMouseOver, and onMouseOut
+      const rootElement = this._shadowRoot.getElementById('root')
+      rootElement.addEventListener('click', this._handleClick.bind(this))
+      rootElement.addEventListener('mouseover', this._handleMouseOver.bind(this))
+      rootElement.addEventListener('mouseout', this._handleMouseOut.bind(this))
     }
 
-    customElements.define('custom-widget', CustomWidget)
+    // Event handler for onClick
+    _handleClick(event) {
+      // Actions to perform when the widget is clicked
+      console.log('Widget clicked!')
+      // Example: Change background color on click
+      event.target.style.backgroundColor = 'lightblue'
+      // Dispatch onClick event
+      this.dispatchEvent(new CustomEvent('onClick'))
+    }
+
+    // Event handler for onMouseOver
+    _handleMouseOver(event) {
+      // Actions to perform when mouse is over the widget
+      console.log('Mouse over the widget!')
+      // Example: Change text color on mouse over
+      event.target.style.color = 'red'
+    }
+
+    // Event handler for onMouseOut
+    _handleMouseOut(event) {
+      // Actions to perform when mouse leaves the widget
+      console.log('Mouse out of the widget!')
+      // Example: Reset text color on mouse out
+      event.target.style.color = 'black'
+    }
+  }
+
+  customElements.define('custom-widget', CustomWidget)
 })()
