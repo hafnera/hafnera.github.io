f08d09b4f2ce9cf3c0aecf998665129306e704de
datobser <159797954+datobser@users.noreply.github.com>
2024-02-29 08:51:31 +0100

    Update mouse.js

--- mouse.js
+++ mouse.js
@@ -11,7 +11,6 @@
         padding: 10px;
         box-sizing: border-box;
         cursor: pointer; /* Add pointer cursor to indicate clickability */
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
       }
       #content {
           position: absolute;
@@ -19,7 +18,8 @@
           left: 10px;
           right: 10px;
           bottom: 10px;
-          background-color: white;          
+          background-color: white; 
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
       }
     </style>
     <div id="root">
