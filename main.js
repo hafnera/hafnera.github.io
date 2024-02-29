6c13bbf1d5a1e922d28875fae696bfc45d855dbc
datobser <159797954+datobser@users.noreply.github.com>
2024-02-29 13:33:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,106 +1,40 @@
-var getScriptPromisify = (src) => {
-    return new Promise(resolve => {
-        $.getScript(src, resolve);
-    });
-};
-
-(function () {
-    const prepared = document.createElement('template');
-    prepared.innerHTML = `
-        <style> 
-        </style>
-        <div id="root" style="width: 100%; height: 100%;"></div>
-    `;
-
-    class SamplePrepared extends HTMLElement {
-        constructor() {
-            super();
-            this._shadowRoot = this.attachShadow({ mode: 'open' });
-            this._shadowRoot.appendChild(prepared.content.cloneNode(true));
-            this._root = this._shadowRoot.getElementById('root');
-            this._props = {};
-            this.render();
-        }
-
-        onCustomWidgetResize(width, height) {
-            this.render();
-        }
-
-        async render(value) {
-            await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js');
-            const chart = echarts.init(this._root, 'dark');
-            const option = {
-                series: [{
-                    type: 'gauge',
-                    detail: { formatter: '{value}%' },
-                    data: [{ value: 50, name: 'completion rate' }]
-                }],
-                startAngle: 200,
-                endAngle: -20,
-                min: 0,
-                max: 100,
-                splitNumber: 12,
-                itemStyle: {
-                    color: '#FFAB91'
-                },
-                progress: {
-                    show: true,
-                    width: 30
-                },
-                pointer: {
-                    show: false
-                },
-                axisLine: {
-                    lineStyle: {
-                        width: 30
-                    }
-                },
-                axisTick: {
-                    distance: -45,
-                    splitNumber: 5,
-                    lineStyle: {
-                        width: 2,
-                        color: '#999'
-                    }
-                },
-                splitLine: {
-                    distance: -52,
-                    length: 14,
-                    lineStyle: {
-                        width: 3,
-                        color: '#999'
-                    }
-                },
-                axisLabel: {
-                    distance: -20,
-                    color: '#999',
-                    fontSize: 20
-                },
-                anchor: {
-                    show: false
-                },
-                title: {
-                    show: false
-                },
-                detail: {
-                    valueAnimation: true,
-                    width: '60%',
-                    lineHeight: 40,
-                    height: '15%',
-                    borderRadius: 8,
-                    offsetCenter: [0, '-15%'],
-                    fontSize: 60,
-                    fontWeight: 'bolder',
-                    formatter: '$ (value)',
-                    color: 'auto'
-                },
-                data: [{
-                    value: value ? value : 0
-                }]
-            };
-            chart.setOption(option);
-        }
-    }
-
-    customElements.define('sample-prepared', SamplePrepared);
+(function() {
+let template = document.createElement("template");
+template.innerHTML = `
+<style>
+:host {
+border-radius: 25px;
+border-width: 4px;
+border-color: black;
+border-style: solid;
+display: block;
+}
+SAP Analytics Cloud Custom Widget Developer Guide
+A Sample Custom Widget: Colored Box PUBLIC 39
+</style>
+`;
+class ColoredBox extends HTMLElement {
+constructor() {
+super();
+let shadowRoot = this.attachShadow({mode: "open"});
+shadowRoot.appendChild(template.content.cloneNode(true));
+this.addEventListener("click", event => {
+var event = new Event("onClick");
+this.dispatchEvent(event);
+});
+this._props = {};
+}
+onCustomWidgetBeforeUpdate(changedProperties) {
+this._props = { ...this._props, ...changedProperties };
+}
+onCustomWidgetAfterUpdate(changedProperties) {
+if ("color" in changedProperties) {
+this.style["background-color"] = changedProperties["color"];
+}
+if ("opacity" in changedProperties) {
+this.style["opacity"] = changedProperties["opacity"];
+}
+}
+}
+customElements.define("com-sap-sample-coloredbox", ColoredBox);
 })();
