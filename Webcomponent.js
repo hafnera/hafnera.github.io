8cf6f60395dd5b9585ec8b5c0a4d7862211d4da1
datobser <159797954+datobser@users.noreply.github.com>
2024-02-29 09:38:42 +0100

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -45,8 +45,8 @@
 
         init() {
 
-            let shadowRoot = this.attachShadow({ mode: "open" });
-            shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._shadowRoot = this.attachShadow({ mode: 'open' });
+            this._shadowRoot.appendChild(template.content.cloneNode(true));
 
             // Add event listener for the resize event
             window.addEventListener('resize', this.onResize.bind(this))
