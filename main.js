62e6d0c98faa8270a6c532b55a6591da0cbd3458
datobser <159797954+datobser@users.noreply.github.com>
2024-03-01 10:19:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,45 @@
 (function() {
-let template = document.createElement("template");
-template.innerHTML = `
-<style>
-:host {
-border-radius: 25px;
-border-width: 4px;
-border-color: black;
-border-style: solid;
-display: block;
-}
-SAP Analytics Cloud Custom Widget Developer Guide
-A Sample Custom Widget: Colored Box PUBLIC 39
-</style>
-`;
-class ColoredBox extends HTMLElement {
-constructor() {
-super();
-let shadowRoot = this.attachShadow({mode: "open"});
-shadowRoot.appendChild(template.content.cloneNode(true));
-this.addEventListener("click", event => {
-var event = new Event("onClick");
-this.dispatchEvent(event);
-});
-this._props = {};
-}
-onCustomWidgetBeforeUpdate(changedProperties) {
-this._props = { ...this._props, ...changedProperties };
-}
-onCustomWidgetAfterUpdate(changedProperties) {
-if ("color" in changedProperties) {
-this.style["background-color"] = changedProperties["color"];
-}
-if ("opacity" in changedProperties) {
-this.style["opacity"] = changedProperties["opacity"];
-}
-}
-}
-customElements.define("com-sap-sample-coloredbox", ColoredBox);
+              let template = document.createElement("template");
+              template.innerHTML = `
+                                    <style>
+                                      :host {
+                                        border-radius: 25px;
+                                        border-width: 4px;
+                                        border-color: black;
+                                        border-style: solid;
+                                        display: block;
+                                      }
+                                    </style>
+                                  `;
+              
+              class ColoredBox extends HTMLElement {
+                  constructor() {
+                      super();
+                      let shadowRoot = this.attachShadow({mode: "open"});
+                      shadowRoot.appendChild(template.content.cloneNode(true));
+                    
+                      this.addEventListener("click", event => {
+                      var event = new Event("onClick");
+                      this.dispatchEvent(event);
+                      });
+                    
+                      this._props = {};
+                  }
+                  
+                  onCustomWidgetBeforeUpdate(changedProperties) {
+                    this._props = { ...this._props, ...changedProperties };
+                  }
+                  
+                  onCustomWidgetAfterUpdate(changedProperties) {
+                    if ("color" in changedProperties) {
+                      this.style["background-color"] = changedProperties["color"];
+                    }
+                    if ("opacity" in changedProperties) {
+                      this.style["opacity"] = changedProperties["opacity"];
+                    }
+                  }
+              
+              }
+  
+  customElements.define("com-sap-sample-coloredbox", ColoredBox);
 })();
