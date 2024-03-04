afbbf940e50b159ebf6fcd9956eab1feb28c19af
datobser <159797954+datobser@users.noreply.github.com>
2024-03-04 15:54:52 +0100

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -11,7 +11,7 @@
             left: 10px;
             right: 10px;
             bottom: 10px;
-            border: 2px solid blue
+            border-color: red;
             background-color: rgba(255, 255, 255, 0.0);
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
         }
