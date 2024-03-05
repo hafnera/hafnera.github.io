a960c861ea8b8beafdfa3561bc5e4332ff375765
datobser <159797954+datobser@users.noreply.github.com>
2024-03-05 11:42:14 +0100

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -16,6 +16,7 @@
             backdrop-filter: blur(10px);
             border: 1px solid rgba(255,255,255,0.25);
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+            opacity: 1;
         }
         </style>
         
