3a5fc2fe47237d3cf44a4cc49ea3cfaafd270271
hafnera <a.hafnerrr@gmail.com>
2024-03-08 15:27:26 +0100

    Update Webcomponent.js
    history version

--- Webcomponent.js
+++ Webcomponent.js
@@ -3,22 +3,19 @@
     tmpl.innerHTML =
         `
         <style>
-            
+        /* CSS styles for the widget */
+    
         #content {
             position: absolute;
             top: 10px;
             left: 10px;
             right: 10px;
             bottom: 10px;
-            border: 1px solid rgba(255,255,255,0.0);
-   
-            background-color: rgba(255, 255, 255);
-            opacity: 0.25;
+            background-color: rgba(255, 255, 255, 0.0);
             -webkit-backdrop-filter: blur(10px);
             backdrop-filter: blur(10px);
-            border-radius: 25;
+            border: 1px solid rgba(255,255,255,0.25);
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-            
         }
         </style>
         
@@ -50,8 +47,7 @@
 
             this.onResize();
         }
-        
-        /*
+
         onCustomWidgetBeforeUpdate(changedProperties) {
             // framework method
             console.log('BeforeUpdate() called');
@@ -67,44 +63,39 @@
             }
 
         }
-        */
-        
+
         onPropertiesChanged(event) {
             console.log('onPropertiesChanged() called with');
             console.log(event.detail.properties);
 
-            const { opacity } = event.detail.properties;
-            this._updateOpacity(opacity);
-
             const { blur } = event.detail.properties;
             this._updateBlur(blur);
+
+            const { opacity } = event.detail.properties;
+            this._updateOpacity(opacity);
 
             const { borderRadius } = event.detail.properties;
             this._updateBorderRadius(borderRadius);
         
             const { shadowSize } = event.detail.properties; // new
             this.updateShadowSize(shadowSize); // new
-            /*
+        
             const { shadowDarkness } = event.detail.properties; // new
             this.updateShadowDarkness(shadowDarkness); // new
-            */
         }
 
         _updateBackgroundColor(color) {
-            //console.log('updateBackgroundColor() called');
             const widget = this._shadowRoot.querySelector('#content');
             widget.style.backgroundColor = color;
         }
 
         _updateOpacity(opacity) {
-            //console.log('updateShadowSize() called');
             const widget = this._shadowRoot.querySelector('#content');
             widget.style.opacity = opacity;
         }
 
 
         _updateBlur(blur) {
-            //console.log('updateBlur() called');
             const widget = this._shadowRoot.querySelector('#content');
             widget.style.webkitBackdropFilter = `blur(${blur}px)`;
             widget.style.backdropFilter = `blur(${blur}px)`;
@@ -112,21 +103,17 @@
 
         _updateBorderRadius(borderRadius) {
             const widget = this._shadowRoot.querySelector('#content');
-            //widget.style.boxShadow = `0 0 10px rgba(0, 0, 0, 0.5)`;
+            widget.style.boxShadow = `0 0 10px rgba(0, 0, 0, 0.5)`;
             widget.style.borderRadius = `${borderRadius}px`;
         }
 
-        updateShadowSize(size) { 
-            //console.log('updateShadowSize() called');
+        updateShadowSize(size) { // new
+            console.log('updateShadowSize() called');
             const widget = this._shadowRoot.querySelector('#content');
             widget.style.boxShadow = `0 0 ${size}px rgba(0, 0, 0, 0.5)`;
         } 
-        
+
         updateShadowDarkness(darkness) {
-            // tried to handle ShadowDarkness as a seperate property
-            // Is not called, because it didn't work like expected
-            // but note: changing the opacity already applies a similar effect
-            
             const widget = this._shadowRoot.querySelector('#content');
             const existingBoxShadow = widget.style.boxShadow;
             const [xOffset, yOffset, blurRadius, spreadRadius, color] = existingBoxShadow.split(' ');
